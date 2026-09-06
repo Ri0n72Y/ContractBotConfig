dedefdaa43187630de92913313fb11cff357cb44
Ri0n72Y <tzy673570751@gmail.com>
2026-09-07 02:40:40 +0800

    client: remove setup launcher

--- client/client-setup.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0client-setup.ps1"
-if errorlevel 1 (
-  echo.
-  echo ContractBot installation failed.
-  pause
-)
-endlocal
